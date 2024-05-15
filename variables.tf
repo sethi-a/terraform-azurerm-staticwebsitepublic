588009cbdddc7bd41dd9067ea6b86bccf6f7240a
sethi-a <64362818+sethi-a@users.noreply.github.com>
2024-05-15 07:49:32 +0000

    Added the default location to the variable

--- variables.tf
+++ variables.tf
@@ -2,13 +2,14 @@
 variable "location" {
   description = "The Azure Region in which all resources groups should be created."
   type = string 
+  default = "australiaeast"
 }
 variable "resource_group_name" {
   description = "The name of the resource group"
   type = string   
     
    validation {
-    condition = length(var.resource_group_name) <= 20
+    condition = length(var.resource_group_name) <= 10
     error_message = "The repository name must be less than or equal to 10 characters."
   }
 }
